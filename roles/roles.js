2236c00a1ddbed196fba3056677b6c5ba8ad13e3
DhibAsma <asma.dhib@esprit.tn>
2022-06-02 17:31:36 +0200

    Create roles.js

---
+++ roles/roles.js
@@ -0,0 +1,26 @@
+import { Role } from 'testcafe';
+const env = require("../config/env.json");
+
+export const roleAdmin = Role(env.adress, async t => {
+    await t
+    .typeText('#at-field-username_and_email', 'admin')
+    .pressKey('tab')
+    .typeText('#at-field-password', 'adminadmin')
+    .click('#at-btn');
+}, { preserveUrl: true });
+
+export const roleUser1 = Role(env.adress, async t => {
+    await t
+    .typeText('#at-field-username_and_email', 'user1')
+    .pressKey('tab')
+    .typeText('#at-field-password', 'user_1')
+    .click('#at-btn');
+}, { preserveUrl: true });
+
+export const roleUser2 = Role(env.adress, async t => {
+    await t
+    .typeText('#at-field-username_and_email', 'user2')
+    .pressKey('tab')
+    .typeText('#at-field-password', 'user_2')
+    .click('#at-btn');
+}, { preserveUrl: true });
