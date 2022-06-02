954bbaec4d8c2fa534270884fe09952a1cda4198
DhibAsma <asma.dhib@esprit.tn>
2022-06-02 17:13:32 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe';
+import  { roleUser1 }  from '../roles/roles';
+
+fixture.disablePageReloads `wekan : check version`
+    .page (env.adress)
+
+    .beforeEach(async t => {
+        await t.useRole(roleUser1);
+    });
+
+    test.disablePageReloads('check version : 1.7', async t => {
+        await t
+        .click('#header-user-bar')
+        .click(Selector('a.js-go-setting'))
+        .click(Selector(".setting-header-btn.informations span").withText("Version"))
+        .expect(Selector("table tr").nth(0).find("td").innerText).eql(env.wekanVersion);
+        });
